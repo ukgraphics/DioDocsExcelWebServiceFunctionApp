9b96bbf64d0b8041de7afd9bd80f821024d4fa18
Kuni <kuni.ujiie@grapecity.com>
2021-10-08 16:18:12 +0900

    プロジェクト ファイルを追加します。

---
+++ DioDocsExcelWebServiceFunctionApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using GrapeCity.Documents.Excel;
+using System;
+
+namespace DioDocsExcelWebServiceFunctionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DioDocs for ExcelでExcelのワークシートにWEBSERVICE、FILTERXML関数を追加します。");
+
+            // トライアル版か製品版のライセンスキーを設定
+            //Workbook.SetLicenseKey("");
+
+            Workbook workbook = new Workbook();
+            IWorksheet ws = workbook.Worksheets[0];
+
+            // WEBSERVICE関数でデータを取得するURL
+            ws.Range["A1"].Value = "URL";
+            ws.Range["A2"].Value = "WEBSERVICE関数でデータを取得するURL";
+            ws.Range["A2"].VerticalAlignment = VerticalAlignment.Center;
+            ws.Range["B1"].Value = "https://rss-weather.yahoo.co.jp/rss/days/3410.xml"; // 仙台
+            //ws.Range["B1"].Value = "https://rss-weather.yahoo.co.jp/rss/days/4410.xml"; // 東京
+
+            // WEBSERVICE関数
+            ws.Range["B2"].Formula = "=WEBSERVICE(B1)";
+            //Wrap the XML string returned by WEBSERVICE Function
+            ws.Range["B2"].WrapText = true;
+            ws.Range["B2"].RowHeight = 200;
+
+            // 動的配列数式を有効にする（二つ目のFILTERXML関数でスピルするため）
+            workbook.AllowDynamicArray = true;
+
+            // FILTERXML関数
+            ws.Range["A4"].Formula = "=FILTERXML(B2, \"//channel/title\")";
+
+            // FILTERXML関数
+            ws.Range["B4"].Formula = "=FILTERXML(B2, \"//channel/item/title\")";
+
+            // Excelファイルに保存
+            workbook.Save("result.xlsx");
+        }
+    }
+}
